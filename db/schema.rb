35279140cb0c81c523fa03d0dd01f82213e18cda
rafaDev <rrg1459@hotmail.com>
2023-11-26 11:53:45 -0400

    Comment schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 2021_01_15_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "canales", force: :cascade do |t|
     t.string "nombre", null: false
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
@@ -41,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_000001) do
     t.integer "porcentaje", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "canal_id"
+    t.integer "canal_id", null: false
   end
 
   create_table "opciones", force: :cascade do |t|
